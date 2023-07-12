5583c3807f005a8af180b14b6e1bf4394ffab8fb
username <useremail>
2023-07-11 17:42:32 -0700

    Add project files.

---
+++ GuessingGame.py
@@ -0,0 +1,8 @@
+favorite_food = input("What's my favorite food? ")
+
+if favorite_food == "Pizza":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing!")
